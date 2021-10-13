b6cfe173473fa3298a239a43edbfb24d75f58112
Charlie Benjafield <me@cbenjafield.com>
2021-10-13 14:59:48 +0100

    Resize article card images

--- resources/views/components/article-card.blade.php
+++ resources/views/components/article-card.blade.php
@@ -1,6 +1,6 @@
 <a href="{{ $article->url ?? '#' }}" class="mb-6 md:mb-0 last:mb-0 block text-left rounded-md overflow-hidden hover:ring-4 hover:ring-gray-900 hover:ring-offset-1 transition duration-150 ease-in-out">
     <div class="h-52 bg-gray-100 dark:bg-gray-700 relative w-full overflow-hidden">
-        <img loading="lazy" src="{{ $article->cover_image_url }}" alt="{{ $article->title }}" class="object-cover object-center min-h-full min-w-full" height="1080" width="1920">
+        <img loading="lazy" src="{{ $article->cover_image_url }}" alt="{{ $article->title }}" class="object-cover object-center min-h-full min-w-full" height="500" width="281">
     </div>
     <div class="p-3 bg-white dark:bg-gray-800">
         <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
